e19fab306d772c46653bb34da676d65a67ac09c9
Vamsi <vk784@snu.edu.in>
2024-05-24 23:48:52 +0530

    Add alu file

---
+++ Week-2/alu.c
@@ -0,0 +1,30 @@
+//#include <stdio.h>
+
+int main()
+{
+    float a;
+    float b;
+    int op;
+  //  printf("Enter number-1 ");
+  //  scanf("%f",&a);
+  //  printf("Enter number-2 ");
+  //  scanf("%f",&b);
+  //  printf("Enter 1,2,3,4 for add,subtract,multiply,divide respectively ");
+  //  scanf("%d",&op);
+    if(op==1){
+    //    printf("Addition sum = %f\n",a+b);
+    }
+    else if(op==2){
+    //    printf("Subtraction difference = %f\n",a-b);
+    }
+    else if(op==3){
+    //    printf("Multiplicatio product = %f\n",a*b);
+    }
+    else if(op==4){
+    //    printf("Division quotient = %f\n",a/b);
+    }
+    else{
+    //    printf("Invalid operation\n");
+    }
+    return 0;
+}
